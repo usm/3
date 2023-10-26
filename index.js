973f8c406b57eca97e214cf931fe8f0ea629ec27
Jonas Almeida <jalmeida@mathbiol.org>
2023-10-26 08:15:46 -0400

    resume dev

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 console.log(`USM3 index.js loaded \n${Date()}`);
 
 (async function(){
-    usmMod = await import('./export.js')
+    USM = (await import('./usm.mjs')).USM
     if(typeof(define)!='undefined'){ // in the odd chance someone is still using require ...
-        define(usmMod)
+        define(USM)
     }
-    u = new usmMod.USM
+    u = new USM
     console.log(u)
 })()
+
+/* Notes
+
+# TP53 variants
+## literature reference
+https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8516647/
+## Database
+https://tp53.isb-cgc.org
+
+
+# COVID-19 SARS-2
+https://gisaid.org/wiv04
+
+*/
 
 
